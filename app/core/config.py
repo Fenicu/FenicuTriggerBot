93cf167c842d0d397f4351a26a6b7978de35a3f5
fenicu <fenicu@fenicu.men>
2026-01-22 22:53:34 +0300

    fix

--- app/core/config.py
+++ app/core/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     URL_PREFIX: str = ""
     BOT_USERNAME: str | None = Field(None, validation_alias="VITE_BOT_USERNAME")
-    GBAN_LIST_URL: str = "https://lols.bot/spam/banlist.jsonn"
+    GBAN_LIST_URL: str = "https://lols.bot/spam/banlist.json"
 
     @computed_field
     def BOT_ADMINS(self) -> list[int]:
